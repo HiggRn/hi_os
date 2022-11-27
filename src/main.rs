e131bac01e66d37b2b516b771f3382fff06e6675
HiggRn <zijie.gu@stu.pku.edu.cn>
2022-11-27 22:59:01 +0800

    Complete till the 'Double Faults' part

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ fn trivial_assertion() {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello world{}", "!");
+    hi_os::init();
     
     #[cfg(test)]
     test_main();
 
+    println!("Succeeded");
     loop {}
 }
 
